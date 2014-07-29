055794a33ba41a34159c50185978fa1f269c95e3
Jacob Wagner <swagner1104@gmail.com>
2014-07-29 11:17:09 -0500

    renamed plays/ to playbooks/, added all-the-things playbook for ease

--- scripts/hosts.py
+++ scripts/hosts.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 import argh
+
+# This will eventually need to get away from chef and to ansible (jwagner)
 from chef import Node, autoconfigure
 
+
 def hosts(env=None, name=None):
-    api = autoconfigure()
+    autoconfigure()
     if name:
         nodes = (node for node in Node.list() if name in Node(node).name)
     else:
-        nodes = (node for node in Node.list() if Node(node).chef_environment == env)
+        nodes = (
+            node for node in Node.list() if Node(node).chef_environment == env)
 
     file = open("hosts", "w")
     file.write("[hosts]\n")
